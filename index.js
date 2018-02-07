308d5325b38cebad554b1967847d97f3ad5bbd93
Hans Gutknecht <hansdotcom@gmail.com>
2018-02-07 16:54:20 -0500

    Service

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const Nightmare = require('nightmare')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/hans', (req, res) => {
+    console.log('oh hai there')
+    const nightmare = Nightmare({ show: true })
+    nightmare
+      .goto('https://duckduckgo.com')
+      .type('#search_form_input_homepage', 'github nightmare')
+      .click('#search_button_homepage')
+      .wait('#r1-0 a.result__a')
+      .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
+      .end()
+      .then(console.log)
+      .catch((error) => {
+        console.error('Search failed:', error);
+      })
+    res.end()
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
